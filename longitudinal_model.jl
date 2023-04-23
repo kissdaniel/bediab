0ec71d7aa9697317697197271b9481c6b123c72f
kissdaniel <kissdanieldezso@gmail.com>
2023-04-23 18:05:33 +0000

    Deploying to gh-pages from @ kissdaniel/bediab@33eba1ee36a006f9e7e69d154d9aaf28a38077c7 🚀

---
+++ longitudinal_model.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.19.25
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 998f7fbe-e1fc-11ed-3ea8-d9f5d7e3f859
+1
+
+# ╔═╡ Cell order:
+# ╠═998f7fbe-e1fc-11ed-3ea8-d9f5d7e3f859
